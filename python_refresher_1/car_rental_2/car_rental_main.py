8b2eab3287eaaf2f911519e21981cce9d2225985
Andres Padilla <andrespadilla2014@gmail.com>
2024-04-07 00:25:57 -0700

    Added Two Cars to Main Function

--- python_refresher_1/car_rental_2/car_rental_main.py
+++ python_refresher_1/car_rental_2/car_rental_main.py
@@ -1,7 +1,14 @@
 #Main Function to Run Car Rental Platform
+import car_rental_functions
+
 
 def main():
-    pass
+    c1 = car_rental_functions.Car('Toyota', 'Camry', 2020, 'Red')
+    c2 = car_rental_functions.Car('Tesla', 'Model Y', 2024, 'White')
+    c1.print_car_info()
+    c1.available_cars()
+    c2.print_car_info()
+    c2.available_cars()
 
 #Start Script Here
 if __name__ == '__main__':
