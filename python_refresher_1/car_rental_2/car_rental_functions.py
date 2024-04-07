a120d5d62953b2dae09c6aed80cc8e6622fd9936
Andres Padilla <andrespadilla2014@gmail.com>
2024-04-07 00:20:09 -0700

    Added Car Class with Constructor and Methods to Print Car Info and Display Number of Available Cars

--- python_refresher_1/car_rental_2/car_rental_functions.py
+++ python_refresher_1/car_rental_2/car_rental_functions.py
@@ -1,6 +1,27 @@
-#Functions and Classes for Car Rental Platform
+# Functions and Classes for Car Rental Platform
 
 import datetime
 
+
 class Car:
-    pass
+    # Variable/Class attribute to track the number of available cars
+    num_cars = 0
+
+    # Constructor
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+        Car.num_cars += 1
+
+    def print_car_info(self):
+        print('Car Information:')
+        print('Make: ', self.make)
+        print('Model: ', self.model)
+        print('Year: ', self.year)
+        print('Color: ', self.color)
+
+    # Method to Number of Available Cars
+    def available_cars(self):
+        print('There are currently ', Car.num_cars, 'cars available')
