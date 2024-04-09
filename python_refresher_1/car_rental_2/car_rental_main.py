6dfbf89da5f7ecd759acd156da7c00fb3f02913b
Andres Padilla <andrespadilla2014@gmail.com>
2024-04-08 22:56:35 -0700

    Added More Car Options: 5 Total Cars

--- python_refresher_1/car_rental_2/car_rental_main.py
+++ python_refresher_1/car_rental_2/car_rental_main.py
@@ -5,10 +5,11 @@ import car_rental_functions
 def main():
     c1 = car_rental_functions.Car('Toyota', 'Camry', 2020, 'Red')
     c2 = car_rental_functions.Car('Tesla', 'Model Y', 2024, 'White')
+    c3 = car_rental_functions.Car('Honda', 'Civic', 2023, 'Black')
+    c4 = car_rental_functions.Car('Nissan', 'Altima', 2019, 'Blue')
+    c5 = car_rental_functions.Car('Ford', 'F-150', 2022, 'Grey')
     c1.print_car_info()
     c1.available_cars()
-    c2.print_car_info()
-    c2.available_cars()
 
 #Start Script Here
 if __name__ == '__main__':
