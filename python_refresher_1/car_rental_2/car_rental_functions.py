584fd5b2cf391ead52ba0156ab428658e4ae003f
Andres Padilla <andrespadilla2014@gmail.com>
2024-04-12 17:49:44 -0700

    Made car_list a Class Attribute and Made Initial return_cars Method

--- python_refresher_1/car_rental_2/car_rental_functions.py
+++ python_refresher_1/car_rental_2/car_rental_functions.py
@@ -1,19 +1,29 @@
 # Functions and Classes for Car Rental Platform
 class Car:
-    # Variable/Class attribute to track the number of available cars
+    # Variable (Class Attribute) to track the number of available cars
     num_cars = 0
+
+    # List (Class Attribute) to Track List of Cars
+    car_list = []
 
     # Constructor
     def __init__(self, make, model, year, color, hourly=30, daily=200, weekly=1000, available=True):
+        ### Car Attributes ###
         self.make = make
         self.model = model
         self.year = year
         self.color = color
+
+        ### Car Rates ###
         self.hourly = hourly
         self.daily = daily
         self.weekly = weekly
+
+        ### Car Availability ###
         self.available = available
-        self.car_list = []
+
+        ### Other Attributes ###
+        Car.car_list.append(self)
         Car.num_cars += 1
 
     # Display Car Information Method
@@ -25,21 +35,12 @@ class Car:
         print('Color:     ', self.color)
         print('Available: ', self.available)
 
-    # Method to Add Car (class) to a List
-    def add_car(self, car):
-        self.car_list.append(car)
-
-    def get_car_list(self):
-        return self.car_list
-
     # Method to Display Number of Available Cars
-    def available_cars(self, car_obj_list):
+    def available_cars(self):
         print('There are currently', Car.num_cars, 'cars available:')
-        for car_obj in car_obj_list:
+        for i, car_obj in enumerate(Car.car_list):
             if car_obj.available:
-                print(
-                    car_obj.year, car_obj.color, car_obj.make, car_obj.model
-                )
+                print(i, '--->', car_obj.year, car_obj.color, car_obj.make, car_obj.model)
 
     # Method to Calculate Car Rental Bill from Hourly Rate
     def rent_hourly(self, hours):
@@ -58,21 +59,27 @@ class Customer(Car):
     def __init__(self, name):
         self.name = name
         self.rented_cars = []
+        self.bill = 0
 
     # Method to Display Customer Info
     def print_customer_info(self):
         print(self.name)
 
-    def retrieve_cars(self, car_list, requested_cars):
-        # Do if requested cars is both positive and less than total available cars
-        if len(car_list) > len(requested_cars) > 0:
+    # Method for Customer to retrieve cars for rent
+    def retrieve_cars(self, requested_cars):
+        # Do if requested cars is both positive and less than or equal to total available cars
+        if len(Car.car_list) >= len(requested_cars) > 0:
             print('Requested the following cars:')
             for i in requested_cars:
-                self.rented_cars.append(car_list[i])
-                car_list[i].available = False
+                self.rented_cars.append(Car.car_list[i])
+                Car.car_list[i].available = False
                 Car.num_cars -= 1
-                print(
-                    car_list[i].year, car_list[i].color, car_list[i].make, car_list[i].model
-                )
+                print(Car.car_list[i].year, Car.car_list[i].color, Car.car_list[i].make, Car.car_list[i].model)
         else:
-            print('Error: Only', len(car_list), 'available to check out')
+            print('Error: Only', len(Car.car_list), 'available to check out')
+
+    def return_cars(self):
+        if len(self.rented_cars) != 0:
+            pass
+        else:
+            print('Error: You have no cars rented out')
--- python_refresher_1/car_rental_2/car_rental_main.py
+++ python_refresher_1/car_rental_2/car_rental_main.py
@@ -11,26 +11,14 @@ def main():
     c4 = car_rental_functions.Car('Nissan', 'Altima', 2019, 'Blue')
     c5 = car_rental_functions.Car('Ford', 'F-150', 2022, 'Grey')
 
-    # Add Cars to a List Using 'add_car' method
-    c1.add_car(c1)
-    c1.add_car(c2)
-    c1.add_car(c3)
-    c1.add_car(c4)
-    c1.add_car(c5)
-
-    # Store List of Cars in a Variable
-    car_list = c1.get_car_list()
-
-    # c1.print_car_info()
-    # c1.available_cars(car_list)
+    c1.print_car_info()
 
     # Customer
     ct1 = car_rental_functions.Customer('Andres')
     ct1.print_customer_info()
-
-    ct1.available_cars(car_list)
-    ct1.retrieve_cars(car_list, [0,2])
-    ct1.available_cars(car_list)
+    ct1.available_cars()
+    ct1.retrieve_cars([0,1,2,3,4,5])
+    ct1.available_cars()
 
 
 # Start Script Here
