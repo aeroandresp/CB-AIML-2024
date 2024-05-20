cab63da970f0d4a131f22b9e58817b5ea5af7f38
Andres Padilla <andrespadilla2014@gmail.com>
2024-05-19 20:01:52 -0700

    Filled Null Incomes with Average Incomes Based off Marital Status and Education

--- data_science_2/marketing_campaigns_2/main.py
+++ data_science_2/marketing_campaigns_2/main.py
@@ -25,13 +25,36 @@ print(missing_values)
 # To Calculate an Average Income for Them
 # df_filled = df.fillna(df.mean())
 
+# Remove Dollar Sign and Comma from String
+income_fix = income.str.replace(r'[$,]+','', regex=True)
+
+# Convert Income Values from Object to Float
+income = income_fix.astype('float')
+
+# Insert FLoat Income into Data Frame
+df['Income'] = income
+print(df['Income'])
 
 # Extract Entries with No Income Info
 empty_entries = df[df['Income'].isnull() == True]
 print(empty_entries['Marital_Status'],empty_entries['Education'])
 
-# Remove Dollar Sign and Comma from String
-income_fix = income.str.replace(r'[$,]+','', regex=True)
+# Options for Marital Status in Missing Income Entries are as Follows:
+# Single, Married, Together, and Widow
+# Options for Education in Missing Income Entries are as Follows:
+# Graduation, Master, PhD, and 2n Cycle
 
-# Convert Income Values from Object to String
-income = income_fix.astype('float')
+# Method for Replacing Null Income Values
+# to Average Incomes Based on Marital
+# Status and Education
+for index, row in empty_entries.iterrows():
+    # print(f"Index: {index}, Marital Status: {row['Marital_Status']}, Education: {row['Education']}")
+    avg_value = df.loc[(df['Marital_Status'] == row['Marital_Status']) &
+    (df['Education'] == row['Education'])]['Income'].mean()
+    # print(avg_value)
+    df.loc[index, 'Income'] = avg_value
+
+# Checking for missing values again
+missing_values = df.isnull().sum()
+print("Missing Values per Column:")
+print(missing_values)
