8bbda60c4af62d2a6da246662c9356ee8d570222
Andres Padilla <andrespadilla2014@gmail.com>
2024-07-10 00:05:40 -0700

    Added Box Plots to Detect Outliers

--- machine_learning_3/creating_cohorts_of_songs_2/main.py
+++ machine_learning_3/creating_cohorts_of_songs_2/main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 # %matplotlib inline
 from sklearn.cluster import KMeans
+
+# Control Variables
+show_plots = True
 
 # Create Data Frame
 df = pd.read_csv('rolling_stones_spotify.csv')
@@ -13,7 +17,24 @@ df = pd.read_csv('rolling_stones_spotify.csv')
 
 # Show info
 print(df.info())
-print(df.head())
+# print(df.head())
 
 # Check for missing values in data
 print(df.isna().sum())
+
+# Drop Unnamed Column
+df1 = df.drop(columns='Unnamed: 0')
+
+# Box Plots
+columns = df1.columns.tolist()
+print(columns)
+
+for col in columns:
+    # print(col, 'is', df1[col].dtypes)
+    if df1[col].dtypes != 'object':
+        sns.boxplot(y=col, data=df1)
+        plt.title('Box Plot')
+        plt.xlabel('All Data')
+        plt.ylabel(col)
+        if show_plots:
+            plt.show()
