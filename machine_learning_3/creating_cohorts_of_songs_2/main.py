b27776f335b8fad06012b4057cf39f341470228f
Andres Padilla <andrespadilla2014@gmail.com>
2024-07-10 23:08:16 -0700

    Checks for Duplicate and Missing Entries

--- machine_learning_3/creating_cohorts_of_songs_2/main.py
+++ machine_learning_3/creating_cohorts_of_songs_2/main.py
@@ -7,7 +7,7 @@ import seaborn as sns
 from sklearn.cluster import KMeans
 
 # Control Variables
-show_plots = True
+show_plots = False
 
 # Create Data Frame
 df = pd.read_csv('rolling_stones_spotify.csv')
@@ -21,14 +21,18 @@ print(df.info())
 # print(df.head())
 
 # Check for missing values in data
+print('Number of missing values for each column:')
 print(df.isna().sum())
 
 # Drop Unnamed Column
 df1 = df.drop(columns='Unnamed: 0')
 
-# Box Plots
+# Check for duplicates
+print('Number of duplicate entries:', df1.duplicated().sum())
+
+# Box plots to detect outliers
 columns = df1.columns.tolist()
-print(columns)
+# print(columns)
 functions.box_plot_outlier(df, columns, show_plots)
 
 
